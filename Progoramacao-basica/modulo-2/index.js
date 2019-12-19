045489d58b0b563858c12fe17574279fee70a0c1
Carine <carine_neris@hotmail.com>
2019-12-18 23:35:40 -0300

    Modulo 2 - Manipulando Elementos e usando Canvas

---
+++ Progoramacao-basica/modulo-2/index.js
@@ -0,0 +1,15 @@
+window.console.log("Inicio do script");
+
+document.write("Está é uma linha de texto");
+document.writeln("Está é uma linha com writeln "); 
+document.write("Outra linha de texto");
+
+/*window.location.reload();  funçao usada para recarregar a pg*/
+
+window.console.log("Cheguei no fim do Script");
+
+
+ /* manipular o elemento*/
+
+var paragrafo = document.getElementById("paragrafo");
+paragrafo.innerHTML = "Manipulando elemento html pelo id"
