17a03a171490e9cd4d60555b814752d321033f7f
carine <carineneris@abaweb.org>
2019-12-19 13:59:12 -0300

    escultando eventos do teclado e do click

--- Progoramacao-basica/modulo-2/canvas/index.js
+++ Progoramacao-basica/modulo-2/canvas/index.js
@@ -3,9 +3,38 @@
 var canvas = document.getElementById("meu-canvas");
 var papel = canvas.getContext("2d");
 
+var teclas = {
+    Up: 37,
+    Rigth: 39,
+    Down: 40,
+    Left: 37
+}
+
 var x = 50;
 var y = 50;
 
+/* Ouvindo Eventos atravez do click do botao */
+var botao = document.getElementById("botao");
+botao.addEventListener("click", desenhaLinhaVermelha);
+
+/* Ouvindo Eventos que vem do document */
+document.addEventListener("keydown", verTecla);
+
+function verTecla(event){
+    /* console.log(event); */
+    
+    switch(event.keyCode){
+        case teclas.Down:
+            console.log("clicou para baixo");
+        break;
+    }
+}
+
+
+
+function desenhaLinhaVermelha(){
+    desenha("red", x , y , 200, 200);
+}
 
 function desenha(cor, xInicial, yInicial, xFinal, yFinal){
     papel.beginPath();
@@ -17,4 +46,4 @@ function desenha(cor, xInicial, yInicial, xFinal, yFinal){
     papel.closePath();
 }
 
-desenha("pink", 10 , 10 , 150, 150);
+desenha("blue", x + 150 , y , 200, 200);
