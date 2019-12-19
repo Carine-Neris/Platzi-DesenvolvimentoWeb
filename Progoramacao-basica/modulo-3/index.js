99386367f3137a18ce5cf58349fdbe0354332574
carine <carineneris@abaweb.org>
2019-12-19 17:08:51 -0300

    aula de estrutura de repeticoes

---
+++ Progoramacao-basica/modulo-3/index.js
@@ -0,0 +1,13 @@
+//número aleatório
+Math.random()
+//retorna um número entre 0 e 0.999
+
+//retorna o piso de um numero
+Math.floor()
+
+//retorna o proximo numero inteiro
+Math.ceil()
+
+//número aleatorio entre x e y
+Math.floor(Math.random() * (x-y) + 1 ) + x
+
---
+++ Progoramacao-basica/modulo-3/projeto-do-modulo/index.js
@@ -0,0 +1,9 @@
+function numeroAleatorio(x,y){
+    return Math.floor(Math.random()*(y-x)+1) + x;
+}
+
+var num = 10;
+
+for(var i=0; num<10; i++){
+    console.log(i);
+}
