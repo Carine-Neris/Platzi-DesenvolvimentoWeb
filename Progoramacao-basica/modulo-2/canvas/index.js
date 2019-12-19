f2b3a0dc21a4b13d67d6aaf2f30177e2a19f580c
carine <carineneris@abaweb.org>
2019-12-19 13:38:05 -0300

    aplicando funções

--- Progoramacao-basica/modulo-2/canvas/index.js
+++ Progoramacao-basica/modulo-2/canvas/index.js
@@ -6,10 +6,15 @@ var papel = canvas.getContext("2d");
 var x = 50;
 var y = 50;
 
-papel.beginPath();
-papel.strokeStyle = "red";
-papel.lineWidth = 3;
-papel.moveTo(x,y);
-papel.lineTo(200,200);
-papel.stroke();
-papel.closePath();
+
+function desenha(cor, xInicial, yInicial, xFinal, yFinal){
+    papel.beginPath();
+    papel.strokeStyle = cor;
+    papel.lineWidth = 3;
+    papel.moveTo(xInicial,yInicial);
+    papel.lineTo(xFinal,yFinal);
+    papel.stroke();
+    papel.closePath();
+}
+
+desenha("pink", 10 , 10 , 150, 150);
