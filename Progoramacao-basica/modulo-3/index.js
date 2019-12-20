cdd45bc88f9243962a8dcba1282467647414bd20
Carine <carine_neris@hotmail.com>
2019-12-19 21:21:32 -0300

    Fim do projeto fazenda platzi

--- Progoramacao-basica/modulo-3/index.js
+++ Progoramacao-basica/modulo-3/index.js
@@ -1,13 +1,24 @@
-//número aleatório
-Math.random()
-//retorna um número entre 0 e 0.999
+/* número aleatório
+    Math.random()
+retorna um número entre 0 e 0.999 */
 
-//retorna o piso de um numero
-Math.floor()
 
-//retorna o proximo numero inteiro
-Math.ceil()
+/* retorna o piso de um numero */
+//Math.floor()
 
-//número aleatorio entre x e y
-Math.floor(Math.random() * (x-y) + 1 ) + x
 
+/* retorna o proximo numero inteiro */
+//Math.ceil()
+
+/* número aleatorio entre x e y */
+//Math.floor(Math.random() * (x-y) + 1 ) + x
+
+function numeroAleatorio(x,y){
+    return Math.floor(Math.random()*(y-x)+1) + x;
+}
+
+var num = 10;
+
+for(var i=0; i < num; i++){
+    console.log(numeroAleatorio(100,200));
+}
--- Progoramacao-basica/modulo-3/projeto-do-modulo/index.js
+++ Progoramacao-basica/modulo-3/projeto-do-modulo/index.js
@@ -1,9 +1,43 @@
+var canvas = document.getElementById("meu-canvas");
+var papel = canvas.getContext("2d");
+
+var fundo = {
+    url:"./imgs/fundo.png"
+}
+
+var vaca = {
+    url:"./imgs/vaca.png"
+}
+
+fundo.imagem = new Image();
+fundo.imagem.src = fundo.url;
+vaca.imagem = new Image();
+vaca.imagem.src = vaca.url;
+
+fundo.imagem.addEventListener("load", carregaFundo);
+vaca.imagem.addEventListener("load", carregaVaca);
+
+function carregaFundo(){
+    papel.drawImage(fundo.imagem, 0, 0);
+    
+}
+
+function carregaVaca(){
+    var numeroVacas = numeroAleatorio(2,10);
+
+    for(var i=0 ; i < numeroVacas; i++){
+        var xA = numeroAleatorio(100,400);
+        var yA = numeroAleatorio(100,400);
+        papel.drawImage(vaca.imagem, xA, yA);
+    }   
+}
+
 function numeroAleatorio(x,y){
     return Math.floor(Math.random()*(y-x)+1) + x;
 }
 
 var num = 10;
 
-for(var i=0; num<10; i++){
-    console.log(i);
+for(var i=0; i < num; i++){
+    console.log(numeroAleatorio(100,200));
 }
