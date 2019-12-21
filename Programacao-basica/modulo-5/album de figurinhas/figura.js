65678498f436a817ecfd9b2ca59c0ca27534d3ae
Carine <carine_neris@hotmail.com>
2019-12-21 13:20:35 -0300

    Finalizado Album

--- Programacao-basica/modulo-5/album de figurinhas/figura.js
+++ Programacao-basica/modulo-5/album de figurinhas/figura.js
@@ -3,9 +3,19 @@ class Figura{
         this.nome = nome;
         this.pais = pais;
         this.posicao = posicao;
+
+        this.imagem = new Image(320, 400);
+        this.imagem.src = urlImagem;
     }
     
     falar(){
         alert("Meu nome é : "+ this.nome);
+    }
+
+    exibir(){
+        document.body.appendChild(this.imagem);
+        document.write("<br/>Nome: " + this.nome + "<br />" );
+        document.write("País: " + this.pais + "<br />" );
+        document.write("Posição: " + this.posicao + "<br />" );
     }
 }
--- Programacao-basica/modulo-5/album de figurinhas/index.js
+++ Programacao-basica/modulo-5/album de figurinhas/index.js
@@ -1,5 +1,20 @@
-var figura =  new Figura("Carine", "Brasil", "Goleira","./imgs/cris.jpeg" );
+var cris =  new Figura("Cristiane", "Brasil", "Atacante","./imgs/cris.jpeg" );
+var marta = new Figura("Marta", "Brasil", "Meio Campo", "./imgs/marta.jpg" );
+var debinha = new Figura("Debinha", "Brasil", "Atacante", "./imgs/debinha.jpg");
 
-figura.falar()
+var colecaoFiguras = [];
+
+colecaoFiguras.push(cris);
+colecaoFiguras.push(marta);
+colecaoFiguras.push(debinha);
+
+console.log(colecaoFiguras.length);
+
+for(var i = 0; i < colecaoFiguras.length; i++){ 
+    colecaoFiguras[i].exibir();
+ }
+
+
+
 
 
